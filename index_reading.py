7ceae1f3c735edc71ddcd34853c65f02ded0bb5a
UsamaMalick <usamamalick22@gmail.com>
2020-03-02 00:18:25 +0500

    Index Reading.

---
+++ index_reading.py
@@ -0,0 +1,22 @@
+
+
+
+path = "term_index.txt"
+file = None
+try:
+    file = open(path , "r")
+except IOError:
+    print("File Error occurred!")
+
+
+content = file.read().split('\n')
+# print(content)
+id = 101
+print(content[id-100])
+# for line in content:
+#     if not line:
+#         break
+#     parsed = line.split()
+#     print(parsed[0] +" : "+ parsed[1] + " : " + parsed[2])
+
+file.close()
